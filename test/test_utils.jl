9a8542c78f6a5642267f6ec3e6cea7cb39d14a9c
Marius <tchiapiyossamariuspaulin@yahoo.fr>
2025-01-28 19:11:22 +0100

    modified:   test/test_utils.jl

--- test/test_utils.jl
+++ test/test_utils.jl
@@ -2,42 +2,40 @@ using Random: seed!, rand
 using DataFrames: DataFrame
 using CairoMakie: LineSegments, save, Lines
 
-
-#generate Data
+# Generate person data as a dictionary
 function create_person_df(n_samples = 10)
-
-    seed!(10)
-    df = DataFrame(
-        height=rand(150:180, n_samples),
-        weight=rand(40:130, n_samples),
-        age=rand(0:70, n_samples), # random numbers between 0 and 70
-        income=rand(450:5000, n_samples),
-        education_years=rand(0:25, n_samples) # random numbers between 0 and 70
+    Random.seed!(10)
+    data = @dict(
+        height = rand(150:180, n_samples),
+        weight = rand(40:130, n_samples),
+        age = rand(0:70, n_samples), # Random numbers between 0 and 70
+        income = rand(450:5000, n_samples),
+        education_years = rand(0:25, n_samples) # Random numbers between 0 and 25
     )
-
-    return df
+    return DataFrame(data)
 end
+
+# Generate car data as a dictionary
 function create_car_df(n_samples = 10)
-
-    seed!(10)
-    df = DataFrame(
-        horsepower=rand(60:300, n_samples),
-        weight=rand(90:2000, n_samples),
-        age=rand(0:70, n_samples)
+    Random.seed!(10)
+    data = @dict(
+        horsepower = rand(60:300, n_samples),
+        weight = rand(90:2000, n_samples),
+        age = rand(0:70, n_samples)
     )
-
-    return df
+    return DataFrame(data)
 end
-#count line segments in a plot
+
+# Count line segments in a plot
 function count_line_segments(fig)
     return length(filter(
-        x->(typeof(x)<:LineSegments),
+        x -> (typeof(x) <: LineSegments),
         fig.scene.plots))
 end
 
-#counts lines except black ones
-function count_lines(fig) 
+# Count lines except black ones
+function count_lines(fig)
     return length(filter(
-        x-> typeof(x)<:Lines && !(x[:color][] == :black),
+        x -> typeof(x) <: Lines && !(x[:color][] == :black),
         fig.scene.plots))
 end
