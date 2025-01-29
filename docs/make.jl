9200be227f772f3d362c0ba6383279a308452ee8
Marius <tchiapiyossamariuspaulin@yahoo.fr>
2025-01-29 21:42:14 +0100

    new file:   docs/build/assets/themes/catppuccin-frappe.css

--- docs/make.jl
+++ docs/make.jl
@@ -1,25 +1,21 @@
-using ParallelPlots
 using Documenter
+using ParallelPlots
 
-DocMeta.setdocmeta!(ParallelPlots, :DocTestSetup, :(using ParallelPlots); recursive=true)
-
-makedocs(;
-    modules=[ParallelPlots],
-    authors="Moritz Schelten <moritz155@win.tu-berlin.de>",
-    sitename="ParallelPlots.jl",
-    format=Documenter.HTML(),
-    pages=[
+makedocs(
+    sitename = "ParallelPlots",
+    format = Documenter.HTML(),
+    pages = [
         "Home" => "index.md",
         "Getting Started" => "getting_started.md",
         "Usage" => "usage.md",
         "Examples" => "examples.md",
-        "Working on ParallelPlots" => "development.md"
-    ],
+        "Development" => "development.md"
+    ]
 )
 
-deploydocs(;
-    repo="github.com/moritz155/ParallelPlots.git",
-    devbranch="main",
+deploydocs(
+    repo = "github.com/moritz155/ParallelPlots.git",
+    devbranch = "main",
     devurl = "dev",
     versions = ["stable" => "v^", "v#.#", "dev" => "dev"]
 )
